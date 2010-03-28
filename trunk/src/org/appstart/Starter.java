42f17d180904dd5b7ddd57e94c0b870b0445b138
mdallav <mdallav@e61cdb65-b83a-bfde-a374-10aea631e9ca>
2010-03-28 15:24:49 +0000

    use splash.img as default splash image animated splash image while launching appstart

--- trunk/src/org/appstart/Starter.java
+++ trunk/src/org/appstart/Starter.java
@@ -1,6 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  The MIT License
+ *
+ *  Copyright 2010 Marco Dalla Vecchia <mdallav@gmail.com>.
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
  */
 package org.appstart;
 
@@ -15,7 +34,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.StringTokenizer;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,6 +43,7 @@ import java.util.logging.Logger;
  * @author marco
  */
 public class Starter {
+    public static final String DEFAULT_SPLASH_IMG = "splash.img";
     static Logger log = Logger.getLogger("AppStart");
 
     public static final String APPSTART_MAIN_CLASS = "app.main.class";
@@ -118,7 +137,7 @@ public class Starter {
         // flag to follow child process output
         Boolean follow = Boolean.parseBoolean(launchProps.getProperty(
                 "app.follow", "false"));
-        File splash = new File(appstartDir, "splash.jpg");
+        File splash = new File(appstartDir, DEFAULT_SPLASH_IMG);
 
         ArrayList<String> cmd = new ArrayList<String>();
         cmd.add(java.getAbsolutePath());
